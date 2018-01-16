f1e9a8dfb0fa8460dd34b3610019033ad3128e2e
klaudiaRajs <150319@uad.ac.uk>
2018-01-16 23:50:38 +0000

    First commit

--- PostboxCommunicator/Form1.cs
+++ PostboxCommunicator/Form1.cs
@@ -12,6 +12,7 @@ namespace PostboxCommunicator {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            string a = "test";
         }
     }
 }
