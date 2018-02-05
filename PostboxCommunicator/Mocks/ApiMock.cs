a4fcfcc750c2982aa09191d3f88bc814433552ff
klaudiaRajs <150319@uad.ac.uk>
2018-02-05 10:51:53 +0000

    Refactoring - removal of unnecessary imports

--- PostboxCommunicator/Mocks/ApiMock.cs
+++ PostboxCommunicator/Mocks/ApiMock.cs
@@ -1,10 +1,6 @@
 ﻿using PostboxCommunicator.Models;
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PostboxCommunicator.Mocks {
     class ApiMock {
--- PostboxCommunicator/Models/UserModel.cs
+++ PostboxCommunicator/Models/UserModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PostboxCommunicator.Models {
+﻿namespace PostboxCommunicator.Models {
     class UserModel {
 
         public string id { get; set; }
