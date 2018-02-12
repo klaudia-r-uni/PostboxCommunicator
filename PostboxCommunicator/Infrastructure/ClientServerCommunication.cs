80b405ea3c3455b69bb5f773f38b5503c7c354b0
Susie Robertson <7252327+SMR10@users.noreply.github.com>
2018-02-12 14:21:38 +0000

    Added some models and logic for messages and client list.

---
+++ PostboxCommunicator/Infrastructure/ClientServerCommunication.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using PostboxCommunicator.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+
+namespace PostboxCommunicator.Infrastructure {
+    class ClientServerCommunication {
+        private static ClientServerCommunication instance;
+        private static HttpClient hClient;
+        public WebSocket sock;
+
+        private ClientServerCommunication() {
+            hClient = new HttpClient();
+            hClient.BaseAddress = new Uri("http://localhost:3000/");
+        }
+
+        //Singleton
+        public static ClientServerCommunication getInstance() {
+            if (instance == null) {
+                instance = new ClientServerCommunication();
+            }
+
+            return instance;
+        }
+
+        public async Task<string> login (LoginModel loginDetails) {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginDetails),
+                Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await hClient.PostAsync("api/login", content);
+            string error = await response.Content.ReadAsStringAsync();
+
+            if (error == "") { 
+                connect();
+                return "true";
+            }
+            return error;
+        }
+
+        private void connect() {
+            sock = new WebSocket("ws://localhost:8080");
+            sock.OnOpen += (sender, e) => {
+                Console.WriteLine("onOpen");
+            };
+            sock.Connect();
+
+
+            sock.OnMessage += (sender, e) => {
+                //method from ui to pass message to.
+            };
+        }
+
+        public void sendMessage(NewMessageModel message){
+            string messageString= JsonConvert.SerializeObject(message);
+            sock.Send(messageString);
+        }
+
+        public async Task<string> getUsers() {
+
+            HttpResponseMessage response = await hClient.GetAsync("api/getUsers");
+            string users = await response.Content.ReadAsStringAsync();
+
+            return users;
+        }
+
+
+
+
+        //connect to server
+        //login
+
+        //if login successful
+        //start web socket connection
+        //successful?
+        //get list of users
+        //broadcast who's online
+
+        //send message
+        //rec message
+    }
+}
--- PostboxCommunicator/Program.cs
+++ PostboxCommunicator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PostboxCommunicator.Infrastructure;
+using PostboxCommunicator.Models;
 
 namespace PostboxCommunicator {
     static class Program {
@@ -11,9 +13,25 @@ namespace PostboxCommunicator {
         /// </summary>
         [STAThread]
         static void Main() {
+            ClientServerCommunication server = ClientServerCommunication.getInstance();
+            LoginModel user = new LoginModel();
+            user.username = "user1";
+            user.password = "user1";
+            string error = server.login(user).Result;
+            string users = server.getUsers().Result;
+
+            NewMessageModel mess = new NewMessageModel();
+            mess.sender = user.username;
+            mess.receiver = "user2";
+            mess.content = "this is a message";
+            mess.dateTime = DateTime.Now;
+
+            server.sendMessage(mess);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
         }
+
     }
 }
